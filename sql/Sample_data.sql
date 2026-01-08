db9541153c6bfeaa8bb31c61286e2e2183ba1545
Sovannary Chhoeurn <rasovanna785@gmail.com>
2026-01-08 08:05:22 +0700

    Add SQL schema and Simple data

---
+++ sql/Sample_data.sql
@@ -0,0 +1,24 @@
+﻿
+
+--Sample Data (Optional but Recommended)
+INSERT INTO categories (name, description)
+VALUES 
+(N'ភេសជ្ជៈ', N'ប្រភេទភេសជ្ជៈ'),
+(N'អាហារ', N'ប្រភេទអាហារ'),
+(N'Electronics', N'Electronic products');
+
+--Products
+INSERT INTO products (name, description, price, image_url, category_id)
+VALUES
+(N'ទូរស័ព្ទ Samsung Galaxy S24', N'ទូរស័ព្ទដៃទំនើបបំផុត', 899.99, 'upload/images/samsung-s24.jpg', 3);
+(N'Laptop', N'Gaming laptop', 1300.00, 'uploads/images/laptop.jpg', 3);
+
+
+UPDATE products
+SET image_url = 'upload/images/samsung-s24.jpg'
+WHERE id = 3;
+
+
+
+
+
---
+++ sql/schema.sql
@@ -0,0 +1,76 @@
+﻿CREATE DATABASE TonaireDigitalDB;
+GO
+USE TonaireDigitalDB;
+GO
+
+
+--Users Table (Authentication)
+CREATE TABLE users (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    email NVARCHAR(255) NOT NULL UNIQUE,
+    password_hash NVARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT GETDATE()
+);
+
+--
+SELECT COLUMN_NAME
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'users';
+
+ALTER TABLE users 
+ADD otp NVARCHAR(10),
+    otp_expiration DATETIME;   
+
+--
+--Categories Table
+CREATE TABLE categories (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name NVARCHAR(255) NOT NULL,
+    description NVARCHAR(500),
+    created_at DATETIME DEFAULT GETDATE()
+);
+
+--Products Table
+CREATE TABLE products (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name NVARCHAR(255) NOT NULL,
+    description NVARCHAR(1000),
+    price DECIMAL(10,2) NOT NULL,
+    image_url NVARCHAR(500),
+    category_id INT NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+
+    CONSTRAINT fk_products_category
+        FOREIGN KEY (category_id)
+        REFERENCES categories(id)
+        ON DELETE CASCADE
+);
+
+--Indexes (Performance + Search)
+CREATE INDEX idx_categories_name ON categories(name);
+CREATE INDEX idx_products_name ON products(name);
+CREATE INDEX idx_products_category ON products(category_id);
+
+
+--Test
+SELECT * FROM users;
+SELECT * FROM categories;
+SELECT * FROM products;
+
+use TonaireDigitalDB 
+go
+
+SELECT * FROM categories
+WHERE name COLLATE Khmer_100_CI_AI LIKE N'%ភ%';
+--How Khmer Search Will Work (IMPORTANT)
+SELECT * FROM products
+WHERE name COLLATE Khmer_100_CI_AI LIKE N'%ភេ%';
+--Sorting:
+ORDER BY name COLLATE Khmer_100_CI_AI
+
+--
+
+--
+UPDATE products
+SET image_url = 'upload/images/samsung-s24.jpg'
+WHERE id = 3;
